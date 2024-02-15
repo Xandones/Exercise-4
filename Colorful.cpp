9930ee622e4a162baa4927989e97d045497682b9
Alexandro Hugo Lopes <karkekoisaaeh@outlook.com>
2024-02-15 11:27:32 -0300

    Added coments

--- Colorful.cpp
+++ Colorful.cpp
@@ -2,7 +2,7 @@
 
 int main() 
 {
-	system("color 8E");
-	system("PAUSE");
+	system("color 8E"); // Changes the color of background to grey and th foregroun (letters color) to light yellow
+	system("PAUSE"); // Makes the system wait for a keyboard input (press any key except special keys)
 	return 0;
 }
